af32e060a051d8ff11e79ebeaecb4d9cb1a8d3a3
Pongthorn Boonrod <pongthorn.bo@gmail.com>
2026-07-03 14:03:03 +0700

    feat(db): initialize tables from SQLAlchemy models

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+
+from app.database import Base, engine
+from app.models import product
 from app.routers import products
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
