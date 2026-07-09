10f55606c2805bb18eefc6f778ec51f5a77b19c8
Pongthorn Boonrod <pongthorn.bo@gmail.com>
2026-07-09 15:39:12 +0700

    feat(docker): add Docker setup for FastAPI app

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/inventory_db"
+DATABASE_URL = "postgresql://postgres:postgres@postgres:5432/inventory_db"
 
 engine = create_engine(DATABASE_URL)
 
