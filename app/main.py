3032c7a7b8be20042985bbfbbcd219d4c9fdc336
Pongthorn Boonrod <pongthorn.bo@gmail.com>
2026-07-14 21:59:18 +0700

    feat(api): configure CORS for local frontend

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.models import product
 from app.routers import products, categories
@@ -6,6 +7,16 @@ from app.routers import products, categories
 # Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+allowed_origins = "http://localhost:5173"
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=allowed_origins, 
+    allow_credentials=True, 
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(products.router)
 app.include_router(categories.router)
